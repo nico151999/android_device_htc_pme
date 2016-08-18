6f8d1665cdfc1661225d20f88daeb9100c3c0124
Nico Feulner (@xda: krassermensch) <nico.feulner@gmail.com>
2016-08-18 15:40:52 +0200

    Make FM Radio be compiled
    It's just for testing purposes. It will most probably just compile the APK and implement it into the ROM but not receive any Radio stations via FM. If you want to try it out, too, also enable FMRadio support in the kernel's defconfig.

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# FMRadio
+PRODUCT_PACKAGES += \
+	FMRADIO
+
 # Browser
 PRODUCT_PACKAGES += \
     Gello
