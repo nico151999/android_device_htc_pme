b6b30d1c6f92bede1af3274e7600bff6fefee3fd
Nico <nico.feulner@gmail.com>
2016-08-31 20:08:53 +0200

    Revert "pme: Add a dummy amplifier HAL"
    This reverts commit c2f28e2a41d38fa1c2e87bca99005e351575bb7f.

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -1,20 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := \
-	liblog libutils libcutils libtinyalsa
-
-LOCAL_C_INCLUDES := \
-        external/tinyalsa/include \
-	hardware/libhardware/include
-
 LOCAL_SRC_FILES := \
-	audio_amplifier.c
-
-LOCAL_MODULE := audio_amplifier.msm8996
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -Werror
-
-include $(BUILD_SHARED_LIBRARY)
+	amp_loader.c
+LOCAL_CFLAGS := -Wall -Wextra -std=c11
+LOCAL_MODULE := amp_loader
+include $(BUILD_EXECUTABLE)
