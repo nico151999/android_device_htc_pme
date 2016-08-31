39de7d61e4dc8e8253e7d4b4e335256703965fa0
Nico <nico.feulner@gmail.com>
2016-08-31 20:08:15 +0200

    Revert "checkpoint"
    This reverts commit 761adc5a80029205b55f4a288eb553d9cd500bf5.

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -1,24 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-#include $(CLEAR_VARS)
-#
-#LOCAL_SHARED_LIBRARIES := \
-#	liblog libutils libcutils libtinyalsa
-#
-#LOCAL_C_INCLUDES := \
-#        external/tinyalsa/include \
-#	hardware/libhardware/include
-#
-#LOCAL_SRC_FILES := \
-#	audio_amplifier.c tfa.c
-#
-#LOCAL_MODULE := audio_amplifier.msm8996
-#LOCAL_MODULE_RELATIVE_PATH := hw
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_CFLAGS = -Werror
-#
-#include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES := \
@@ -29,11 +10,11 @@ LOCAL_C_INCLUDES := \
 	hardware/libhardware/include
 
 LOCAL_SRC_FILES := \
-	play.c tfa9888-debug.c tfa.c
+	audio_amplifier.c tfa.c
 
-LOCAL_MODULE := play
+LOCAL_MODULE := audio_amplifier.msm8996
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS = -Werror
 
-include $(BUILD_EXECUTABLE)
-
+include $(BUILD_SHARED_LIBRARY)
