b93d4d552d6e5ff2743e4eae2facad47c3575efe
Nico <nico.feulner@gmail.com>
2016-09-04 09:56:39 +0200

    reverting some changes
    Change-Id: I242f6fd84d214457111b9c814557dc01f58fca7d

--- device.mk
+++ device.mk
@@ -66,7 +66,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio_amplifier.msm8996 \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8996 \
@@ -87,11 +86,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/amplifier/tfa9888_init.asq:system/etc/tfa9888_init.asq \
-    $(LOCAL_PATH)/amplifier/tfa9888_enable.asq:system/etc/tfa9888_enable.asq \
-    $(LOCAL_PATH)/amplifier/tfa9888_disable.asq:system/etc/tfa9888_disable.asq
 
 # FMRadio
 PRODUCT_PACKAGES += \
