643be37150e6e7302b05797d8d3283429bfb2ca3
Nico Feulner <nico.feulner@gmail.com>
2016-09-24 14:32:57 +0200

    pme: fix a typo in crpalmer's 'libacdbloader wrapper'
    Change-Id: I59b9ee1079b3ae935c458051e53d466d8c93c482

--- libacdbloader/Android.mk
+++ libacdbloader/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := acdbloader.c
 LOCAL_MODULE := libacdbloader
-OCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS = -Werror
 
 include $(BUILD_SHARED_LIBRARY)
