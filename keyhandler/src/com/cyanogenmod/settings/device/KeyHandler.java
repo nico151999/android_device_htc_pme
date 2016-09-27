2769b286553195405afbd6a9e8b2cd19c9a3123d
Nico Feulner <nico.feulner@gmail.com>
2016-09-27 16:27:59 +0200

    Update pme: Initial KeyHandler implementation for HTC FPC
    Change-Id: I2c7c5b3c18853f5374c7048d2a7530ec11194bf1

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -18,6 +18,7 @@ package com.cyanogenmod.settings.device;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -45,31 +46,36 @@ public class KeyHandler implements DeviceKeyHandler {
     }
 
     public boolean handleKeyEvent(KeyEvent event) {
+        boolean goHome = false;
+        long[] hapticPattern;
+        if (event.getKeyCode() != KeyEvent.KEYCODE_WAKEUP) {
+            return false;
+        }
         if (event.getAction() != KeyEvent.ACTION_DOWN) {
             return false;
         }
-        if (event.getKeyCode() == KeyEvent.KEYCODE_WAKEUP) {
-            Log.v(TAG, "Detected KEYCODE_WAKEUP, emulating KEYCODE_HOME");
-            doHapticFeedback();
+        /*
+         * TODO: Fix long press detection, add double tap detection
+         */
+        if ((event.getFlags() & KeyEvent.FLAG_LONG_PRESS) != 0) {
+            hapticPattern = getLongIntArray(mContext.getResources(),
+                com.android.internal.R.array.config_longPressVibePattern);
+        } else {
+            // Assume single press
+            goHome = true;
+            hapticPattern = getLongIntArray(mContext.getResources(),
+                com.android.internal.R.array.config_virtualKeyVibePattern);
+        }
+        doHapticFeedback(hapticPattern);
+        if (goHome) {
             launchHomeIntent();
-            return true;
         }
-        return false;
+        return true;
     }
 
-    private void launchHomeIntent() {
-        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
-        homeIntent.addCategory(Intent.CATEGORY_HOME);
-        try {
-            mContext.startActivityAsUser(homeIntent, null, new UserHandle(UserHandle.USER_CURRENT));
-        } catch (Exception e) {
-            Log.e(TAG, "Unable to launch home intent");
-        }
-    }
-
-    private void doHapticFeedback() {
-        final long[] pattern = {0, 25, 8}; // config_virtualKeyVibePattern
-        final boolean hapticsDisabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+    private void doHapticFeedback(long[] pattern) {
+        final boolean hapticsDisabled =
+                Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.HAPTIC_FEEDBACK_ENABLED, 0, UserHandle.USER_CURRENT) == 0;
         if (mVibrator == null) {
             return;
@@ -84,5 +90,28 @@ public class KeyHandler implements DeviceKeyHandler {
             // Pattern vibration
             mVibrator.vibrate(pattern, -1);
         }
+    }
+
+    private void launchHomeIntent() {
+        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory(Intent.CATEGORY_HOME);
+        try {
+            mContext.startActivityAsUser(homeIntent, null,
+                    new UserHandle(UserHandle.USER_CURRENT));
+        } catch (Exception e) {
+            Log.e(TAG, "Unable to launch home intent");
+        }
+    }
+
+    private long[] getLongIntArray(Resources r, int resid) {
+        int[] ar = r.getIntArray(resid);
+        if (ar == null) {
+            return null;
+        }
+        long[] out = new long[ar.length];
+        for (int i=0; i<ar.length; i++) {
+            out[i] = ar[i];
+        }
+        return out;
     }
 }
