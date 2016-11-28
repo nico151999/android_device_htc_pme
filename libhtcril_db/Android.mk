1d3ae42d07b8ab522d27196ab489364da837b3d1
Nico Feulner <nico.feulner@gmail.com>
2016-11-28 18:46:23 +0100

    pme: revert some of the latest commits
    Change-Id: I1c8f158d62cadc5dca8f3fce023e9dde9513ce20

--- libhtcril_db/Android.mk
+++ libhtcril_db/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,13 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/htc/pme/full_pme.mk)
-$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := pme
-PRODUCT_NAME := bs_pme
-PRODUCT_BRAND := HTC
-PRODUCT_MODEL := HTC 10
-PRODUCT_MANUFACTURER := HTC
-PRODUCT_RELEASE_NAME := pme
+LOCAL_SRC_FILES := \
+    htcril_db.c
+
+LOCAL_SHARED_LIBRARIES := libcutils libsqlite
+
+LOCAL_MODULE := libhtcril_db
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := htcril_db_test.c
+LOCAL_MODULE := htcril_db_test
+LOCAL_SHARED_LIBRARIES := libhtcril_db
+include $(BUILD_EXECUTABLE)
